600fbdf4c57ddf32e2639a774a9e49824c0ae15d
Lukas Rügner <ruegnerlukas@gmail.com>
2018-04-20 20:07:52 +0200

    Add test and misc files

--- src/com/ruegnerlukas/Test.java
+++ src/com/ruegnerlukas/Test.java
@@ -8,7 +8,10 @@ import java.awt.event.KeyListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
@@ -45,77 +48,102 @@ public class Test {
 	public static void main(String[] args) {
 
 		
-		Scene sceneA = new Scene() {
-			@Override public void load() {
-				System.out.println("Scene A: load");
-			}
-
-			@Override public void update(int deltaMS) {
-				System.out.println("Scene A: update " + deltaMS + "ms");
-			}
-
-			@Override public void unload() {
-				System.out.println("Scene A: unload");
-			}
-		};
-		
-		Scene sceneB = new Scene() {
-			@Override public void load() {
-				System.out.println("Scene B: load");
-			}
-			@Override public void update(int deltaMS) {
-				System.out.println("Scene B: update " + deltaMS + "ms");
-			}
-			@Override public void unload() {
-				System.out.println("Scene B: unload");
-			}
-		};
-		
-		ColorFadeTransition transitionA = new ColorFadeTransition(2000, new Vector4f(0,0,0,1), new Vector4f(1,1,1,0), new InterpolationSine());
-		ColorFadeTransition transitionB = new ColorFadeTransition(2000, new Vector4f(1,1,1,0), new Vector4f(0,0,0,1), new InterpolationSine());
-
-		
-		SceneManager.get().addScene("sceneA", sceneA);
-		SceneManager.get().addScene("sceneB", sceneB);
-		SceneManager.get().changeScene("sceneA", null, null);
-		
-		long time = 0;
-		boolean changed = false;
-		
-		while(running) {
-			
-			System.out.println();
-			
-			if(time >= 3000 && !changed) {
-				SceneManager.get().changeScene("sceneB", transitionA, transitionB);
-				changed = true;
-			}
-			
-			SceneManager.get().update(50);
-			
-			try {
-				Thread.sleep(50);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			time += 50;
-		}
-		
-		
+//		Scene sceneA = new Scene() {
+//			@Override public void load() {
+//				System.out.println("Scene A: load");
+//			}
+//
+//			@Override public void update(int deltaMS) {
+//				System.out.println("Scene A: update " + deltaMS + "ms");
+//			}
+//
+//			@Override public void unload() {
+//				System.out.println("Scene A: unload");
+//			}
+//		};
 //		
-//		TMXMap tmxMap = TMXLoader.loadTMX(new File("res/unbenannt3.tmx"));
-//		tmxMap.prettyPrint(0);
+//		Scene sceneB = new Scene() {
+//			@Override public void load() {
+//				System.out.println("Scene B: load");
+//			}
+//			@Override public void update(int deltaMS) {
+//				System.out.println("Scene B: update " + deltaMS + "ms");
+//			}
+//			@Override public void unload() {
+//				System.out.println("Scene B: unload");
+//			}
+//		};
 //		
+//		ColorFadeTransition transitionA = new ColorFadeTransition(2000, new Vector4f(0,0,0,1), new Vector4f(1,1,1,0), new InterpolationSine());
+//		ColorFadeTransition transitionB = new ColorFadeTransition(2000, new Vector4f(1,1,1,0), new Vector4f(0,0,0,1), new InterpolationSine());
+//
 //		
+//		SceneManager.get().addScene("sceneA", sceneA);
+//		SceneManager.get().addScene("sceneB", sceneB);
+//		SceneManager.get().changeScene("sceneA", null, null);
 //		
-//		set = TilesetLoader.createTileset(tmxMap.tilesets.get(0));
-//		map = TilemapLoader.createTilemap(tmxMap);
+//		long time = 0;
+//		boolean changed = false;
 //		
-//		try {
-//			tilesetImg = ImageIO.read(new File("res/"+tmxMap.tilesets.get(0).image.source));
-//		} catch (IOException e1) {
-//			e1.printStackTrace();
+//		while(running) {
+//			
+//			System.out.println();
+//			
+//			if(time >= 3000 && !changed) {
+//				SceneManager.get().changeScene("sceneB", transitionA, transitionB);
+//				changed = true;
+//			}
+//			
+//			SceneManager.get().update(50);
+//			
+//			try {
+//				Thread.sleep(50);
+//			} catch (InterruptedException e) {
+//				e.printStackTrace();
+//			}
+//			time += 50;
 //		}
+//		
+		
+		TMXLoader loader = new TMXLoader() {
+			@Override
+			public String onRequestFileContent(String strFile) {
+				BufferedReader br = null;
+				try {
+					br = new BufferedReader(new FileReader("res/"+strFile));
+				} catch (FileNotFoundException e1) {
+					e1.printStackTrace();
+				}
+				StringBuilder sb = new StringBuilder();
+			    String line;
+				try {
+					line = br.readLine();
+				    while (line != null) {
+				        sb.append(line);
+				        sb.append(System.lineSeparator());
+				        line = br.readLine();
+				    }
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+
+			    String everything = sb.toString();
+				return everything;
+			}
+		};
+		TMXMap tmxMap = loader.loadTMX(new File("res/unbenannt3.tmx"));
+		tmxMap.prettyPrint(0);
+		
+		
+		
+		set = TilesetLoader.createTileset(tmxMap.tilesets.get(0));
+		map = TilemapLoader.createTilemap(tmxMap);
+		
+		try {
+			tilesetImg = ImageIO.read(new File("res/"+tmxMap.tilesets.get(0).image.source));
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
 //		
 //		setupDisplay();
 //		
